74a5f5fd148b9372c05f2813b5a625f3b448878d
Raphael Bost <raphael_bost@alumni.brown.edu>
2021-12-04 22:32:57 +0100

    Solve Day 2 Part 1

---
+++ day_2.1/src/main.rs
@@ -0,0 +1,48 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+enum Command {
+    Forward(u32),
+    Down(u32),
+    Up(u32),
+}
+
+#[derive(Clone, Default)]
+struct Location {
+    pos: u32,
+    depth: u32,
+}
+
+impl Location {
+    fn apply(mut self, command: Command) -> Self {
+        // let mut copy = self.clone();
+        match command {
+            Command::Forward(x) => self.pos += x,
+            Command::Down(x) => self.depth += x,
+            Command::Up(x) => self.depth = self.depth.saturating_sub(x),
+        }
+        self
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let filename = &args[1];
+    // Open the file in read-only mode (ignoring errors).
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let loc = reader
+        .lines()
+        .map(|line| match line.unwrap().split_once(' ').unwrap() {
+            (s, v) if s == "forward" => Command::Forward(v.parse::<u32>().unwrap()),
+            (s, v) if s == "down" => Command::Down(v.parse::<u32>().unwrap()),
+            (s, v) if s == "up" => Command::Up(v.parse::<u32>().unwrap()),
+            _ => unreachable!("Invalid input"),
+        })
+        .fold(Location::default(), |loc, command| loc.apply(command));
+
+    println!("Result (pos x depth): {}", loc.pos * loc.depth);
+}
