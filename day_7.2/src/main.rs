1ce2471dae45b2989966fc498ce0795c151b10d8
Raphael Bost <raphael_bost@alumni.brown.edu>
2021-12-07 10:40:18 +0100

    Solve Day 7 Part 2

---
+++ day_7.2/src/main.rs
@@ -0,0 +1,37 @@
+use std::{
+    env,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn compute_fuel_cost(crab_pos: &Vec<u32>, target_pos: u32) -> u32 {
+    crab_pos
+        .iter()
+        .map(|&p| (p as i32 - target_pos as i32).abs() as u32)
+        .map(|f| ((f + 1) * f / 2)) // change this formula for part 2
+        .sum()
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    // Open the file in read-only mode (ignoring errors).
+    let file = File::open(filename).unwrap();
+    let mut reader = BufReader::new(file);
+
+    let n_days = 256;
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+
+    let positions: Vec<u32> = line.split(',').map(|v| v.parse().unwrap()).collect();
+
+    let min = *positions.iter().min().unwrap();
+    let max = *positions.iter().max().unwrap();
+
+    let (opt_p, min_f) = (min..=max)
+        .map(|target| (target, compute_fuel_cost(&positions, target)))
+        .min_by(|(_, f1), (_, f2)| f1.cmp(f2))
+        .unwrap();
+
+    println!("Optimal position {}, fuel {}", opt_p, min_f);
+}
